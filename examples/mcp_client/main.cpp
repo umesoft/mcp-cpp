e7dab2fc43a03dd10969c2cc5f94614fc4a0ac0a
koichi tsubai <tsubai@umesoft.co.jp>
2025-09-07 21:36:24 +0900

    ツール呼び出しを試作

--- examples/mcp_client/main.cpp
+++ examples/mcp_client/main.cpp
@@ -9,15 +9,35 @@ int main()
     Mcp::McpClient client;
     client.Test();
     */
-
+    
     openai::start("", "", true, "http://127.0.0.1:1234/v1/");
 
-    auto chat = openai::chat().create(R"(
-    {
-        "model": "phi-4",
-        "messages":[{"role":"user", "content":"hello!"}]
-    }
-    )"_json);
+    auto chat = openai::chat().create(
+        R"(
+            {
+                "model": "phi-4",
+                "messages":[{"role":"user", "content":"現在の時刻を教えて"}],
+                "tools": [
+                    {
+                        "type": "function",
+                        "function" : {
+                            "name": "get_current_time",
+                            "description": "Get the current time",
+                            "parameters": {
+                                "type": "object",
+                                "properties": {
+                                    "timezone": {
+                                        "type": "string",
+                                        "description": "(Optional) The timezone to get the current time for, e.g. 'America/New_York'"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        )"_json
+    );
     std::cout << "Response is:\n" << chat.dump(2) << '\n'; 
 
     return 0;
