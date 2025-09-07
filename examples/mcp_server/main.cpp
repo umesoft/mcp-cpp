b1950a159c5b3423a07564d33adff6a6ec530bb8
koichi tsubai <tsubai@umesoft.co.jp>
2025-09-07 14:10:56 +0900

    MCP Server実装を反映

--- examples/mcp_server/main.cpp
+++ examples/mcp_server/main.cpp
@@ -1,8 +1,82 @@
+﻿/*
+ *  Copyright (C) 2025 UmeSoftware LLC
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "mcp-cpp/mcp_server.h"
+
+using namespace Mcp;
 
 int main()
 {
-    Mcp::McpServer server;
-    server.Test();
-    return 0;
+	McpServer server("MCP Test");
+
+    /*
+	server.SetAuthorization(
+		"\"https://***tenant name***.us.auth0.com\"",
+		"\"***api permission***\""
+	);
+    */
+
+	server.AddTool(
+		"get_channels",
+		"Returns a list of available TV channels.",
+		std::vector<McpServer::McpProperty> {
+			{ "location", McpServer::PROPERTY_STRING, "location of TV", true }
+		},
+		std::vector<McpServer::McpProperty> {
+			{ "channel_no", McpServer::PROPERTY_STRING, "channel no", true },
+			{ "service_name", McpServer::PROPERTY_STRING, "service name", true }
+		},
+		[](const std::map<std::string, std::string>& args) -> std::vector<McpServer::McpContent> {
+			std::vector<McpServer::McpContent> contents;
+
+			McpServer::McpContent content{
+				.property_type = McpServer::PROPERTY_OBJECT,
+				.value = ""
+			};
+
+			content.properties.push_back({
+				.property_name = "channel_no",
+				.value = "011"
+				});
+			content.properties.push_back({
+				.property_name = "service_name",
+				.value = "NHK G"
+				});
+			contents.push_back(content);
+
+			content.properties.push_back({
+				.property_name = "channel_no",
+				.value = "021"
+				});
+			content.properties.push_back({
+				.property_name = "service_name",
+				.value = "ETV"
+				});
+			contents.push_back(content);
+
+			return contents;
+		}
+	);
+
+	server.Run(
+		// "http://localhost:8000/mcp",
+		"https://localhost:8000/mcp",
+		10 * 60 * 1000
+	);
+
+	return 0;
 }
--- include/mcp-cpp/mcp_server.h
+++ include/mcp-cpp/mcp_server.h
@@ -1,12 +1,107 @@
+/*
+ *  Copyright (C) 2025 UmeSoftware LLC
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
+
+#include <functional>
+#include <map>
+#include <string>
+#include <vector>
 
 namespace Mcp {
 
-class McpServer {
+class McpServer 
+{
 public:
-    McpServer();
+	McpServer(const char* server_name);
 
-    void Test();
+	enum PropertyType {
+		PROPERTY_NUMBER = 1,
+		PROPERTY_STRING,
+		PROPERTY_OBJECT
+	};
+	struct McpProperty {
+		std::string property_name;
+		PropertyType property_type;
+		std::string description;
+		bool required;
+	};
+	struct McpPropertyValue {
+		std::string property_name;
+		std::string value;
+	};
+	struct McpContent {
+		PropertyType property_type;
+		std::string value;
+		std::vector<McpPropertyValue> properties;
+	};
+
+	void SetAuthorization(
+		const char* authorization_servers,
+		const char* scopes_supported
+	);
+
+	void AddTool(
+		const char* tool_name, 
+		const char* tool_description, 
+		const std::vector<McpProperty>& input_schema,
+		const std::vector<McpProperty>& output_schema,
+		std::function <std::vector<McpContent>(const std::map<std::string, std::string>& args)> callback
+		);
+
+	bool Run(const char* url, ulong session_timeout);
+
+private:
+	std::string m_server_name;
+	bool m_authorization;
+	std::string m_authorization_servers;
+	std::string m_scopes_supported;
+	std::string m_url;
+	std::string m_host;
+	std::string m_entry_point;
+
+	bool UpdateUrlPath(const char* url);
+
+	struct McpTool {
+		std::string name;
+		std::string description;
+		std::map<std::string, McpProperty> input_schema;
+		std::map<std::string, McpProperty> output_schema;
+		std::function <std::vector<McpContent>(const std::map<std::string, std::string>& args)> callback;
+	};
+	std::map<std::string, McpTool> m_tools;
+
+	static std::string GetPropertyType(PropertyType type);
+	static std::string GetPropertyValue(const McpTool& tool, McpPropertyValue type, bool escape);
+
+	std::map<std::string, long> m_sessions;
+
+	bool IsEnableSessionId(std::string session_id);
+	void EraseSession(std::string session_id);
+	void ClearSession();
+
+	void* m_rpc_head;
+
+	static void cbEvHander(void* connection, int event_code, void* event_data);
+	static void cbTimerHandler(void* timer_data);
+	static void cbInitialize(void* rpc_req);
+	static void cbLoggingSetLevel(void* rpc_req);
+	static void cbToolsList(void* rpc_req);
+	static void cbToolsCall(void* rpc_req);
 };
 
 }
