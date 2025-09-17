26aa99e999c99a8adb97cc9d165ea2917753a5d0
koichi tsubai <tsubai@umesoft.co.jp>
2025-09-17 10:38:49 +0900

    test agent func

--- examples/mcp-client/main.cpp
+++ examples/mcp-client/main.cpp
@@ -21,21 +21,74 @@
 
 using namespace Mcp;
 
-void test()
+void test_assistant()
 {
-    openai::start("", "", true, "http://127.0.0.1:1234/v1/");
+    openai::start("", "", true, "http://172.16.2.229:1234/v1/");
 
-    auto chat = openai::chat().create(R"(
+	auto request = R"(
+	    {
+	        "model": "gpt-oss-20B",
+	        "messages": [
+	        	{
+	        		"role": "system", 
+	        		"content": "You are a helpful assistant."
+	        	},
+	        	{
+	        		"role": "user", 
+	        		"content": "What time is it now in San Diego?"
+	        	}
+	        ],
+			"tools": [
+	    		{
+	      			"type": "function",
+					"function": {
+						"name": "get_current_time",
+				        "description": "Get the current time in UTC",
+        				"parameters": {
+          					"type": "object",
+	          				"properties": {},
+			    	      	"required": []
+						}
+					}
+				}
+			],
+			"tool_choice": "auto"
+		}
+	)"_json;
+
+    auto response = openai::chat().create(request);
+    std::cout << "Response is:\n" << response.dump(2) << '\n';
+    
+    auto choices = response["choices"];
+    for(auto it = choices.begin(); it != choices.end(); it++)
     {
-        "model": "phi-4",
-        "messages":[{"role":"user", "content":"hello!"}]
-    }
-    )"_json);
-    std::cout << "Response is:\n" << chat.dump(2) << '\n';
+    	auto message = (*it)["message"];
+		request["messages"].push_back(message);
+
+	    auto tool_calls = message["tool_calls"];
+	    for(auto it2 = tool_calls.begin(); it2 != tool_calls.end(); it2++)
+	    {
+			auto func_output = R"(
+		    	{
+		    		"role": "tool", 
+		    		"tool_call_id": "",
+		    		"content": "2025-09-17 10:15:23 UTC"
+				}
+			)"_json;
+			func_output["tool_call_id"] = (*it2)["id"];
+			request["messages"].push_back(func_output);
+			break;
+		}
+		break;
+	}
+	
+    auto response2 = openai::chat().create(request);
+    std::cout << "Response is:\n" << response2.dump(2) << '\n';
 }
 
 int main()
 {
+	/*
 	std::shared_ptr<McpHttpClientTransport> transport = std::move(McpHttpClientTransport::CreateInstance(
         "http://localhost:8000", 
         "/mcp")
@@ -52,6 +105,9 @@ int main()
     client->ToolsCall("count_down", args);
 	
     client->Shutdown();
-
+	*/
+	
+	test_assistant();
+	
     return 0;
 }
